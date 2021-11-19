e2da90f2882808ac756c9380774948024ae96eb4
Andrew Huynh <a5.t.huynh@gmail.com>
2021-11-18 16:21:38 -0800

    Basic query + serialization of history db

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use rusqlite::{params, Connection, OpenFlags, Result};
+
+const HISTORY_DB: &str = "_data/places.sqlite";
+
+#[derive(Debug)]
+struct Place {
+    id: i32,
+    url: String
+}
+
+
+fn main() -> Result<()> {
+    let conn = Connection::open_with_flags(&HISTORY_DB, OpenFlags::SQLITE_OPEN_READ_ONLY)?;
+    println!("Connected to db...");
+
+    let mut stmt = conn.prepare("SELECT id, url FROM moz_places LIMIT 10")?;
+    let place_iter = stmt.query_map(params![], |row| {
+        Ok(Place {
+            id: row.get(0)?,
+            url: row.get(1)?
+        })
+    })?;
+
+    for place in place_iter {
+        println!("Found place {:?}", place.unwrap());
+    }
+
+    Ok(())
 }
