1bec0ff85beb79af91ef84e53073fbf450548880
Andrew Huynh <a5.t.huynh@gmail.com>
2022-01-05 22:37:46 -0800

    Creating db/tables to keep track of robots/fetched pages

--- carto/src/lib.rs
+++ carto/src/lib.rs
@@ -1,22 +1,65 @@
 use directories::ProjectDirs;
 use reqwest::StatusCode;
+use rusqlite::{Connection, OpenFlags, Result};
 use std::{fs, path::PathBuf};
 
 pub mod models;
 use models::Place;
 
 pub struct Carto {
+    db: Connection,
     data_dir: PathBuf,
 }
 
 impl Carto {
+    pub fn init_db(&self, ) -> Result<()> {
+        // Initialize robots table
+        self.db.execute(
+            "CREATE TABLE IF NOT EXISTS robots_txt (
+                id INTEGER PRIMARY KEY,
+                domain TEXT UNIQUE,
+                no_index BOOLEAN,
+                disallow TEXT,
+                allow TEXT,
+                created_at DATETIME,
+                updated_at DATETIME
+            )", []
+        )?;
+
+        // Initialize fetch history table
+        self.db.execute(
+            "CREATE TABLE IF NOT EXISTS fetch_history(
+                id INTEGER PRIMARY KEY,
+                url TEXT UNIQUE,
+                hash TEXT,
+                status INTEGER,
+                no_index BOOLEAN,
+                created_at DATETIME,
+                updated_at DATETIME
+            )", []
+        )?;
+
+        Ok(())
+    }
+
     pub fn init() -> Self {
         let proj_dirs = ProjectDirs::from("com", "athlabs", "carto").unwrap();
         let data_dir = proj_dirs.data_dir().join("crawls");
 
         fs::create_dir_all(&data_dir).expect("Unable to create crawl folder");
 
-        Carto { data_dir }
+        let db_path = proj_dirs.data_dir().join("db.sqlite");
+        dbg!(&db_path);
+        let db = Connection::open_with_flags(
+            &db_path,
+            OpenFlags::SQLITE_OPEN_READ_WRITE |
+            OpenFlags::SQLITE_OPEN_CREATE
+        ).unwrap();
+
+        let carto = Carto { db, data_dir };
+        carto.init_db().expect("Unable to initialize db");
+
+        carto
     }
 
     // TODO: Load web indexing as a plugin?
@@ -28,6 +71,8 @@ impl Carto {
             fs::create_dir(&domain_dir).expect("Unable to create dir");
         }
 
+        // Check for robots.txt of this domain
+
         log::info!("Fetching page: {}", place.url.as_str());
         let res = reqwest::get(place.url.as_str()).await.unwrap();
         log::info!("Status: {}", res.status());
@@ -38,5 +83,8 @@ impl Carto {
             let file_path = domain_dir.join("raw.html");
             fs::write(file_path, body).expect("Unable to save html");
         }
+
+        // Update fetch history
+
     }
 }
--- carto/src/models.rs
+++ carto/src/models.rs
@@ -1,7 +1,39 @@
+use chrono::prelude::*;
+use regex::Regex;
 use url::Url;
 
 #[derive(Debug)]
 pub struct Place {
     pub id: i32,
     pub url: Url,
+}
+
+pub struct RobotsTxt {
+    id: u64,
+    domain: String,
+    no_index: bool,
+    disallow: Vec<Regex>,
+    allow: Vec<Regex>,
+    created_at: DateTime<Utc>,
+    updated_at: DateTime<Utc>,
+}
+
+/// When a URL was last fetched. Also used as a queue for the indexer to determine
+/// what paths to index next.
+#[derive(Debug)]
+pub struct FetchHistory {
+    /// Arbitrary id for this.
+    id: u64,
+    /// URL fetched.
+    url: Url,
+    /// Hash used to check for changes.
+    hash: u64,
+    /// HTTP status when last fetching this page.
+    status: u8,
+    /// Ignore this URL in the future.
+    no_index: bool,
+    /// When this was first added to our fetch history
+    created_at: DateTime<Utc>,
+    /// When this URL was last fetched.
+    updated_at: DateTime<Utc>,
 }
