b10066f75917984b57c0c6e0fbe3835d3673f30a
Andrew Huynh <a5.t.huynh@gmail.com>
2022-03-08 21:38:16 -0800

    🧑‍🎨 Cleaning up styling

--- client/public/glue.js
+++ client/public/glue.js
@@ -1,5 +1,9 @@
-const invoke = window.__TAURI__.invoke;
-const listen = window.__TAURI__.event.listen;
+let invoke = () => {};
+let listen = () => {};
+if (window.__TAURI__) {
+    invoke = window.__TAURI__.invoke;
+    listen = window.__TAURI__.event.listen;
+}
 
 export async function onClearSearch(callback) {
     await listen('clear_search', callback);
