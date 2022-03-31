04e2641c8c95e35944d957f631d16c5541c6940c
Andrew Huynh <a5.t.huynh@gmail.com>
2022-03-31 13:38:34 -0700

    [scraper] Attempt to parse content from page based on <p> nodes

--- crates/spyglass/src/scraper/mod.rs
+++ crates/spyglass/src/scraper/mod.rs
@@ -10,6 +10,7 @@ use std::collections::{HashMap, HashSet};
 use crate::scraper::element::Node;
 use crate::scraper::html::Html;
 
+#[derive(Debug)]
 pub struct ScrapeResult {
     pub title: Option<String>,
     // Page description, extracted from meta tags or summarized from the actual content
@@ -19,24 +20,70 @@ pub struct ScrapeResult {
     pub links: HashSet<String>,
 }
 
+/// Walk the DOM and grab all the p nodes
+fn filter_p_nodes(root: &NodeRef<Node>, p_list: &mut Vec<String>) {
+    for child in root.children() {
+        let node = child.value();
+        if node.is_element() {
+            let element = node.as_element().unwrap();
+            if element.name().eq_ignore_ascii_case("p") {
+                let mut p_content = String::from("");
+                let mut _links = HashSet::new();
+                filter_text_nodes(&child, &mut p_content, &mut _links);
+
+                if !p_content.is_empty() {
+                    p_list.push(p_content);
+                }
+            }
+        }
+
+        if child.has_children() {
+            filter_p_nodes(&child, p_list);
+        }
+    }
+}
+
 /// Filters a DOM tree into a text document used for indexing
-fn filter_text_nodes(
-    root: &NodeRef<Node>,
-    doc: &mut String,
-    links: &mut HashSet<String>,
-    ignore_list: &HashSet<String>,
-) {
+fn filter_text_nodes(root: &NodeRef<Node>, doc: &mut String, links: &mut HashSet<String>) {
+    // TODO: move to config file? turn into a whitelist?
+    // TODO: Ignore list could also be updated per domain as well if needed
+    let ignore_list: HashSet<String> = HashSet::from([
+        "head".into(),
+        "sup".into(),
+        // Ignore elements that often don't contain relevant info
+        "header".into(),
+        "footer".into(),
+        "nav".into(),
+        // form elements
+        "label".into(),
+        "textarea".into(),
+        // Ignore javascript/style nodes
+        "script".into(),
+        "noscript".into(),
+        "style".into(),
+    ]);
+
     let href_key = QualName::new(None, ns!(), local_name!("href"));
+    let role_key = QualName::new(None, ns!(), local_name!("role"));
 
     for child in root.children() {
         let node = child.value();
         if node.is_text() {
-            doc.push('\n');
             doc.push_str(node.as_text().unwrap());
         } else if node.is_element() {
             // Ignore elements on the ignore list
             let element = node.as_element().unwrap();
             if ignore_list.contains(&element.name()) {
+                continue;
+            }
+
+            // Ignore elements whose role is "navigation"
+            // TODO: Filter out full-list of ARIA roles that are not content
+            if element.attrs.contains_key(&role_key)
+                && (element.attrs.get(&role_key).unwrap().to_string() == *"navigation"
+                    || element.attrs.get(&role_key).unwrap().to_string() == *"contentinfo"
+                    || element.attrs.get(&role_key).unwrap().to_string() == *"button")
+            {
                 continue;
             }
 
@@ -50,7 +97,17 @@ fn filter_text_nodes(
             }
 
             if child.has_children() {
-                filter_text_nodes(&child, doc, links, ignore_list);
+                filter_text_nodes(&child, doc, links);
+                // Add spacing to elements that naturally have spacing
+                if element.name().eq_ignore_ascii_case("p")
+                    || element.name().eq_ignore_ascii_case("h1")
+                    || element.name().eq_ignore_ascii_case("h2")
+                    || element.name().eq_ignore_ascii_case("h3")
+                    || element.name().eq_ignore_ascii_case("h4")
+                    || element.name().eq_ignore_ascii_case("h5")
+                {
+                    doc.push(' ');
+                }
             }
         }
     }
@@ -58,20 +115,6 @@ fn filter_text_nodes(
 
 /// Filters a DOM tree into a text document used for indexing
 pub fn html_to_text(doc: &str) -> ScrapeResult {
-    // TODO: move to config file? turn into a whitelist?
-    // TODO: Ignore list could also be updated per domain as well if needed
-    let ignore_list = HashSet::from([
-        // TODO: Parse meta tags
-        "head".into(),
-        // Ignore elements that often don't contain relevant info
-        "header".into(),
-        "footer".into(),
-        "nav".into(),
-        // Ignore javascript nodes
-        "script".into(),
-        "noscript".into(),
-    ]);
-
     let parsed = Html::parse(doc);
     let root = parsed.tree.root();
     let meta = parsed.meta();
@@ -79,16 +122,21 @@ pub fn html_to_text(doc: &str) -> ScrapeResult {
 
     let mut content = String::from("");
     let mut links = HashSet::new();
-    filter_text_nodes(&root, &mut content, &mut links, &ignore_list);
+    filter_text_nodes(&root, &mut content, &mut links);
 
     let description = {
         if meta.contains_key("description") {
             meta.get("description").unwrap().to_string()
         } else if meta.contains_key("og:description") {
             meta.get("og:description").unwrap().to_string()
-        } else if content.len() > 0 {
-            // Extract first paragraph from content
-            todo!()
+        } else if !content.is_empty() {
+            // Extract first paragraph from content w/ text to use as the description
+            let mut p_list = Vec::new();
+            filter_p_nodes(&root, &mut p_list);
+
+            let text = p_list.iter().find(|content| !content.is_empty());
+
+            text.unwrap_or(&String::from("")).trim().to_owned()
         } else {
             "".to_string()
         }
@@ -109,11 +157,28 @@ mod test {
 
     #[test]
     fn test_html_to_text() {
-        let html = include_str!("../../../../fixtures/raw.html");
+        let html = include_str!("../../../../fixtures/html/raw.html");
         let doc = html_to_text(html);
         assert_eq!(doc.title, Some("Old School RuneScape Wiki".to_string()));
         assert_eq!(doc.meta.len(), 9);
         assert!(doc.content.len() > 0);
         assert_eq!(doc.links.len(), 58);
+    }
+
+    #[test]
+    fn test_description_extraction() {
+        let html = include_str!("../../../../fixtures/html/wikipedia_entry.html");
+        let doc = html_to_text(html);
+
+        assert_eq!(
+            doc.title.unwrap(),
+            "Rust (programming language) - Wikipedia"
+        );
+        assert_eq!(doc.description, "Rust is a multi-paradigm, general-purpose programming language designed for performance and safety, especially safe concurrency. Rust is syntactically similar to C++, but can guarantee memory safety by using a borrow checker to validate references. Rust achieves memory safety without garbage collection, and reference counting is optional. Rust has been called a systems programming language, and in addition to high-level features such as functional programming it also offers mechanisms for low-levelmemory management.");
+
+        let html = include_str!("../../../../fixtures/html/personal_blog.html");
+        let doc = html_to_text(html);
+        // ugh need to fix this
+        assert_eq!(doc.description, "2020 July 15 - San Francisco |855 words");
     }
 }
