2dd7722c7e20065b4c7f29473cc9266ac38c1b9b
Andrew Huynh <a5.t.huynh@gmail.com>
2022-04-29 18:44:00 -0700

    Move up schema check/creation to right after we connect to db

--- crates/spyglass/src/state.rs
+++ crates/spyglass/src/state.rs
@@ -23,22 +23,21 @@ impl AppState {
             .await
             .expect("Unable to connect to database");
 
+        let _ = setup_schema(&db.clone())
+            .await
+            .expect("Unable to setup schema");
+
         let index = Searcher::with_index(&IndexPath::LocalPath(Config::index_dir()));
 
         // TODO: Load from saved preferences
         let app_state = DashMap::new();
         app_state.insert("paused".to_string(), "false".to_string());
 
-        let app = AppState {
+        AppState {
             db: db.clone(),
             app_state: Arc::new(app_state),
             config,
             index: Arc::new(Mutex::new(index)),
-        };
-        let _ = setup_schema(&db.clone())
-            .await
-            .expect("Unable to setup schema");
-
-        app
+        }
     }
 }
