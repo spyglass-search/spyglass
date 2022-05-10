0282f669bb073ef3da348a776f9f546190214a00
Andrew Huynh <a5.t.huynh@gmail.com>
2022-05-09 18:25:19 -0700

    Paginate large calls to CDX server using streaming futures 🔥

--- crates/spyglass/src/crawler/bootstrap.rs
+++ crates/spyglass/src/crawler/bootstrap.rs
@@ -7,24 +7,23 @@
 /// TODO: When a lens directory is created, 2 & 3 can be done by our
 /// machines and the pre-processed files can be downloaded without crawling.
 use chrono::{Duration, Utc};
-use reqwest::StatusCode;
+use futures::{FutureExt, Stream, StreamExt};
+use reqwest::{Client, Error};
+use std::collections::{HashMap, HashSet};
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use tokio_retry::strategy::ExponentialBackoff;
+use tokio_retry::Retry;
 use url::Url;
 
 use entities::models::crawl_queue;
 use entities::sea_orm::DatabaseConnection;
 use shared::config::UserSettings;
 
+// Using Internet Archive's CDX because it's faster & more reliable.
 const ARCHIVE_CDX_ENDPOINT: &str = "https://web.archive.org/cdx/search/cdx";
 const ARCHIVE_WEB_ENDPOINT: &str = "https://web.archive.org/web";
-
-// http://web.archive.org/cdx/search/cdx?
-//      url=roll20.net/compendium/dnd5e
-//      &matchType=prefix
-//      &filter=statuscode:200
-//      &filter=mimetype:text/html
-//      &collapse=urlkey
-//      &showResumeKey=true
-//      &fl=original
 
 fn create_archive_url(url: &str) -> String {
     // Always try to grab the latest archived crawl
@@ -37,41 +36,123 @@ fn create_archive_url(url: &str) -> String {
     )
 }
 
-/// Check CDX index for a list of URLs that match the prefix
-async fn fetch_cdx(prefix: &str) -> anyhow::Result<Vec<String>> {
-    let client = reqwest::Client::new();
+type CDXResponse = Result<String, Error>;
+pub struct CDXStream {
+    pub client: Client,
+    pub is_done: bool,
+    pub limit: usize,
+    pub params: HashMap<String, String>,
+    pub next_page_request: Option<Pin<Box<dyn Future<Output = CDXResponse>>>>,
+}
 
-    let last_year = Utc::now() - Duration::weeks(52);
-    let last_year = last_year.format("%Y").to_string();
+impl CDXStream {
+    pub fn new(client: &Client, prefix: &str, limit: usize) -> Self {
+        let last_year = Utc::now() - Duration::weeks(52);
+        let last_year = last_year.format("%Y").to_string();
 
-    // Docs on what the CDX server supports:
-    // https://github.com/internetarchive/wayback/tree/master/wayback-cdx-server#filtering
-    let params = vec![
-        // TODO: Make this configurable in the lens format?
-        ("matchType", "prefix"),
-        // Only successful pages
-        ("filter", "statuscode:200"),
-        // Only HTML docs
-        ("filter", "mimetype:text/html"),
-        // Remove dupes
-        ("collapse", "urlkey"),
-        // If there are too many URLs, let's us paginate
-        ("showResumeKey", "true"),
-        // Only care about the original URL crawled
-        ("fl", "original"),
-        // Only look at things that have existed within the last year.
-        ("from", &last_year),
-        ("url", prefix),
-    ];
+        let params: HashMap<String, String> = HashMap::from([
+            // TODO: Make this configurable in the lens format?
+            ("matchType".into(), "prefix".into()),
+            // Only successful pages
+            ("filter".into(), "statuscode:200".into()),
+            // Only HTML docs
+            ("filter".into(), "mimetype:text/html".into()),
+            // Remove dupes
+            ("collapse".into(), "urlkey".into()),
+            // If there are too many URLs, let's us paginate
+            ("showResumeKey".into(), "true".into()),
+            ("limit".into(), limit.to_string()),
+            // Only care about the original URL crawled
+            ("fl".into(), "original".into()),
+            // Only look at things that have existed within the last year.
+            ("from".into(), last_year),
+            ("url".into(), prefix.into()),
+        ]);
 
-    let resp = client.get(ARCHIVE_CDX_ENDPOINT).query(&params);
-    let resp = resp.send().await?;
-    if resp.status() != StatusCode::OK {
-        return Ok(Vec::new());
+        let client_clone = client.clone();
+        CDXStream {
+            client: client.clone(),
+            limit,
+            is_done: false,
+            params: params.clone(),
+            next_page_request: Some(Box::pin(async move {
+                fetch_cdx_page(client_clone, params).await
+            })),
+        }
     }
+}
 
-    let content = resp.text().await?;
-    Ok(content.split('\n').map(|x| x.to_string()).collect())
+impl Stream for CDXStream {
+    type Item = anyhow::Result<HashSet<String>>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let inner = self.get_mut();
+
+        if inner.is_done || inner.next_page_request.is_none() {
+            return Poll::Ready(None);
+        }
+
+        if let Some(mut future) = inner.next_page_request.take() {
+            return match future.poll_unpin(cx) {
+                Poll::Ready(res) => match res {
+                    Ok(response) => {
+                        let mut urls = HashSet::new();
+                        let mut resume_key = None;
+
+                        for url in response.split('\n') {
+                            if url.is_empty() {
+                                continue;
+                            }
+
+                            // Text after the limit num is the resume key
+                            if urls.len() >= inner.limit {
+                                resume_key = Some(url.to_string());
+                            } else {
+                                urls.insert(url.into());
+                            }
+                        }
+
+                        if let Some(resume_key) = resume_key {
+                            inner.params.insert("resumeKey".into(), resume_key);
+                            let client = inner.client.clone();
+                            let params = inner.params.clone();
+                            let fetch =
+                                Box::pin(async move { fetch_cdx_page(client, params).await });
+                            inner.next_page_request = Some(fetch);
+                        } else {
+                            inner.is_done = true;
+                        }
+
+                        Poll::Ready(Some(Ok(urls)))
+                    }
+                    Err(err) => {
+                        inner.is_done = true;
+                        Poll::Ready(Some(Err(err.into())))
+                    }
+                },
+                Poll::Pending => {
+                    inner.next_page_request = Some(future);
+                    Poll::Pending
+                }
+            };
+        }
+
+        Poll::Pending
+    }
+}
+
+async fn fetch_cdx_page(client: Client, params: HashMap<String, String>) -> CDXResponse {
+    let retry_strat = ExponentialBackoff::from_millis(1000).take(3);
+
+    Retry::spawn(retry_strat, || async {
+        let req = client.get(ARCHIVE_CDX_ENDPOINT).query(&params);
+        let resp = req.send().await;
+        match resp {
+            Ok(resp) => resp.text().await,
+            Err(err) => Err(err),
+        }
+    })
+    .await
 }
 
 pub async fn bootstrap(
@@ -82,21 +163,26 @@ pub async fn bootstrap(
     // Check for valid URL and normalize it.
     let url = Url::parse(url)?;
 
-    // Extract URLs
-    let urls = fetch_cdx(url.as_str()).await?;
+    let client = reqwest::Client::new();
+    let mut stream = CDXStream::new(&client, &url.to_string(), 1337);
 
-    // Add URLs to crawl queue
+    // Extract URLs
     let mut count = 0;
     let overrides = crawl_queue::EnqueueSettings {
         skip_blocklist: true,
         crawl_type: crawl_queue::CrawlType::Bootstrap,
     };
 
-    for url in urls.iter() {
-        let archive_url = create_archive_url(url);
-        match crawl_queue::enqueue(db, &archive_url, settings, &overrides).await? {
-            Some(_) => {}
-            None => count += 1,
+    while let Some(result) = stream.next().await {
+        if let Ok(urls) = result {
+            // Add URLs to crawl queue
+            for url in urls.iter() {
+                let archive_url = create_archive_url(url);
+                match crawl_queue::enqueue(db, &archive_url, settings, &overrides).await? {
+                    Some(_) => {}
+                    None => count += 1,
+                }
+            }
         }
     }
 
@@ -105,21 +191,12 @@ pub async fn bootstrap(
 
 #[cfg(test)]
 mod test {
-    use super::{bootstrap, fetch_cdx};
-
+    use super::{bootstrap, CDXStream};
     use entities::models::crawl_queue;
     use entities::test::setup_test_db;
+    use futures::StreamExt;
 
     use shared::config::{Limit, UserSettings};
-
-    #[tokio::test]
-    #[ignore]
-    async fn test_cdx_query() {
-        let res = fetch_cdx("https://roll20.net/compendium/dnd5e")
-            .await
-            .unwrap();
-        assert_eq!(res.len(), 3777);
-    }
 
     #[tokio::test]
     #[ignore]
@@ -131,10 +208,25 @@ mod test {
         };
 
         let res = bootstrap(&db, &settings, "https://roll20.net/compendium/dnd5e").await;
-        assert_eq!(res.unwrap(), 1935);
+        assert_eq!(res.unwrap(), 2269);
         let num_queue = crawl_queue::num_queued(&db, crawl_queue::CrawlStatus::Queued)
             .await
             .unwrap();
-        assert_eq!(num_queue, 1935);
+        assert_eq!(num_queue, 2269);
+    }
+
+    #[tokio::test]
+    #[ignore]
+    async fn test_stream() {
+        let client = reqwest::Client::new();
+        let mut stream = CDXStream::new(&client, "https://roll20.net/compendium/dnd5e", 100);
+
+        let mut all_urls = Vec::new();
+        while let Some(results) = stream.next().await {
+            let page = results.unwrap();
+            all_urls.extend(page.clone());
+        }
+
+        assert_eq!(all_urls.len(), 2275);
     }
 }
