b3b3109178e64e0183f8016071c7b0486a156fa4
Andrew Huynh <a5.t.huynh@gmail.com>
2022-05-17 21:58:04 -0700

    Fixing search box overflow

--- crates/client/src/pages/search.rs
+++ crates/client/src/pages/search.rs
@@ -158,7 +158,7 @@ pub fn search_page() -> Html {
                     ref={(*query_ref).clone()}
                     id={"searchbox"}
                     type={"text"}
-                    class={"bg-neutral-800 text-white text-5xl p-4 flex-1 focus:outline-none"}
+                    class={"bg-neutral-800 text-white text-5xl p-4 overflow-hidden flex-1 focus:outline-none"}
                     placeholder={"Search"}
                     {onkeyup}
                     {onkeydown}
