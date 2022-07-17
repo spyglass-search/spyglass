7e77dfd69e45d40ee39fe9736b211a87891fc62e
Andrew Huynh <a5.t.huynh@gmail.com>
2022-07-17 16:26:49 -0700

    cargo clippy

--- crates/spyglass-plugin/src/shims.rs
+++ crates/spyglass-plugin/src/shims.rs
@@ -4,8 +4,8 @@ use std::io;
 use crate::{PluginEnqueueRequest, PluginMountRequest};
 
 /// Add an item to the Spyglass crawl queue
-pub fn enqueue_all(urls: &Vec<String>) {
-    if object_to_stdout(&PluginEnqueueRequest { urls: urls.clone() }).is_ok() {
+pub fn enqueue_all(urls: &[String]) {
+    if object_to_stdout(&PluginEnqueueRequest { urls: urls.to_owned() }).is_ok() {
         unsafe {
             plugin_enqueue();
         }
