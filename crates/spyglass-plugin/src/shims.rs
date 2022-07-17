9c9b197a4224fe3bd149a87e76268b63eebb4161
Andrew Huynh <a5.t.huynh@gmail.com>
2022-07-17 10:11:53 -0700

    docs: adding docstrings for exported funcs

--- crates/spyglass-plugin/src/shims.rs
+++ crates/spyglass-plugin/src/shims.rs
@@ -3,20 +3,29 @@ use std::io;
 
 use crate::{PluginEnqueueRequest, PluginMountRequest};
 
+/// Add an item to the Spyglass crawl queue
 pub fn enqueue(url: String) {
     if object_to_stdout(&PluginEnqueueRequest { url }).is_ok() {
-        unsafe { plugin_enqueue() }
+        unsafe {
+            plugin_enqueue();
+        }
     }
 }
 
+/// Utility function to log to spyglass logs
 pub fn log(msg: String) {
     println!("{}", msg);
-    unsafe { plugin_log() }
+    unsafe {
+        plugin_log();
+    }
 }
 
+/// Adds / updates a file in the plugin VFS from the host.
 pub fn sync_file(dst: String, src: String) {
     if object_to_stdout(&PluginMountRequest { dst, src }).is_ok() {
-        unsafe { plugin_sync_file() }
+        unsafe {
+            plugin_sync_file();
+        }
     }
 }
 
