5b3f456b276c0a9c6c7970f294f6f54731a97cb0
Andrew Huynh <a5.t.huynh@gmail.com>
2022-08-23 09:14:57 -0700

    feature: add spyglass payment link (#176)

--- crates/shared/src/constants.rs
+++ crates/shared/src/constants.rs
@@ -1,2 +1,3 @@
 pub const GITHUB_REPO_URL: &str = "https://github.com/a5huynh/spyglass";
 pub const DISCORD_JOIN_URL: &str = "https://discord.gg/663wPVBSTB";
+pub const PAYMENT_URL: &str = "https://buy.stripe.com/00g0216rG5ow3MkdQQ";
