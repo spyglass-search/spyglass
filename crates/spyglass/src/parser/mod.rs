9efc19142ce4165d15cddd8edad9c35192e23f3d
JB <jbredeso@gmail.com>
2022-10-22 21:55:55 -0700

    Initial support for xlsx, xls and ods spreadsheets

--- crates/spyglass/src/parser/mod.rs
+++ crates/spyglass/src/parser/mod.rs
@@ -6,13 +6,19 @@ use std::{
 };
 
 mod docx_parser;
+mod xlsx_parser;
 
 /*
  * Processes the file extension to identify if there is a special
  * parser available
  */
 pub fn supports_filetype(extension: &OsStr) -> bool {
-    if extension.eq_ignore_ascii_case("docx") {
+    print!("Extension {:?}", extension);
+    if extension.eq_ignore_ascii_case("docx")
+        || extension.eq_ignore_ascii_case("xlsx")
+        || extension.eq_ignore_ascii_case("xls")
+        || extension.eq_ignore_ascii_case("ods")
+    {
         return true;
     }
     false
@@ -24,6 +30,11 @@ pub fn supports_filetype(extension: &OsStr) -> bool {
 pub fn parse_file(extension: &OsStr, file_path: &Path) -> io::Result<String> {
     if extension.eq_ignore_ascii_case("docx") {
         return docx_parser::parse(file_path);
+    } else if extension.eq_ignore_ascii_case("xlsx")
+        || extension.eq_ignore_ascii_case("xls")
+        || extension.eq_ignore_ascii_case("ods")
+    {
+        return xlsx_parser::parse(file_path);
     }
     Err(Error::new(
         ErrorKind::Unsupported,
