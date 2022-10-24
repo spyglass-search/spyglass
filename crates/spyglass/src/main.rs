a8f7202fe17a2171f6c316c75490525d85b95098
Andrew Huynh <a5.t.huynh@gmail.com>
2022-10-24 13:48:24 -0700

    only set libspyglass to debug level logging

--- crates/spyglass/src/main.rs
+++ crates/spyglass/src/main.rs
@@ -18,11 +18,11 @@ use shared::config::Config;
 
 mod api;
 
-#[cfg(not(debug_assertions))]
 const LOG_LEVEL: tracing::Level = tracing::Level::INFO;
-
+#[cfg(not(debug_assertions))]
+const SPYGLASS_LEVEL: &str = "libspyglass=INFO";
 #[cfg(debug_assertions)]
-const LOG_LEVEL: tracing::Level = tracing::Level::DEBUG;
+const SPYGLASS_LEVEL: &str = "libspyglass=DEBUG";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = Config::new();
@@ -47,6 +47,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with(
             EnvFilter::from_default_env()
                 .add_directive(LOG_LEVEL.into())
+                .add_directive(SPYGLASS_LEVEL.parse().expect("Invalid EnvFilter"))
+                // Don't need debug/info level logging for these
                 .add_directive("tantivy=WARN".parse().expect("Invalid EnvFilter"))
                 .add_directive("regalloc=WARN".parse().expect("Invalid EnvFilter"))
                 .add_directive("cranelift_codegen=WARN".parse().expect("Invalid EnvFilter"))
