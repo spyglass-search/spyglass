264315ab2bf06917f25d4601400ff353a0f4d043
Andrew Huynh <a5.t.huynh@gmail.com>
2022-11-29 12:33:33 -0800

    use our own tauri fork to expose & handle RunEvent::ApplicationShouldHandleReopen (#240)

--- crates/tauri/src/platform/mac.rs
+++ crates/tauri/src/platform/mac.rs
@@ -1,40 +1,7 @@
-use cocoa::appkit::{NSApp, NSApplication, NSEvent, NSEventMask, NSEventSubtype};
-use cocoa::base::nil;
-use cocoa::foundation::{NSAutoreleasePool, NSDate, NSString};
-
 use tauri::Window;
 
 use crate::window;
 use shared::event::ClientEvent;
-
-/// Poll for dock events
-pub fn poll_app_events(window: &Window) {
-    unsafe {
-        let _pool = NSAutoreleasePool::new(nil);
-        let ns_event = NSApp().nextEventMatchingMask_untilDate_inMode_dequeue_(
-            NSEventMask::NSAppKitDefinedMask.bits(),
-            NSDate::distantPast(cocoa::base::nil),
-            // Use custom event loop name so we don't trampled on others.
-            NSString::alloc(nil).init_str("spyglassEventLoop"),
-            cocoa::base::YES,
-        );
-
-        if ns_event == nil || ns_event.eventType() as u64 == 21 {
-            return;
-        }
-
-        let subtype = ns_event.subtype();
-        match subtype {
-            NSEventSubtype::NSApplicationActivatedEventType => {
-                window::show_search_bar(window);
-            }
-            NSEventSubtype::NSApplicationDeactivatedEventType => {
-                window::hide_search_bar(window);
-            }
-            _ => {}
-        }
-    }
-}
 
 pub fn show_search_bar(window: &Window) {
     let _ = window.show();
