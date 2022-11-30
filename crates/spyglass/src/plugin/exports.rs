dd2cf5a983c5be4e13820651ff86c4ea2b1624b0
Andrew Huynh <a5.t.huynh@gmail.com>
2022-11-29 21:54:14 -0800

    bugfix: handle ignore on file changes (#242)
    * bugfix: take into account ignore files when looking @ file change notifications
    
    * remove hostname plugin envvar, not used
    
    * apply "files" lens to file URIs enqueued by the local-file-importer

--- crates/spyglass/src/plugin/exports.rs
+++ crates/spyglass/src/plugin/exports.rs
@@ -181,9 +181,16 @@ fn handle_plugin_enqueue(env: &PluginEnv, urls: &Vec<String>) {
     let rt = tokio::runtime::Handle::current();
     let urls = urls.clone();
 
+    // Hacky way to apply lenses to enqueues from the plugins.
     let mut tags = vec![(TagType::Source, env.name.clone())];
-    if env.name == "chrome-importer" || env.name == "firefox-importer" {
-        tags.push((TagType::Lens, "bookmarks".to_owned()));
+    match env.name.as_str() {
+        "chrome-importer" | "firefox-importer" => {
+            tags.push((TagType::Lens, "bookmarks".to_owned()));
+        }
+        "local-file-importer" => {
+            tags.push((TagType::Lens, "files".to_owned()));
+        }
+        _ => {}
     }
 
     rt.spawn(async move {
--- crates/spyglass/src/plugin/mod.rs
+++ crates/spyglass/src/plugin/mod.rs
@@ -6,6 +6,7 @@ use std::time::Duration;
 
 use dashmap::DashMap;
 use entities::sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
+use ignore::WalkBuilder;
 use notify::{event::ModifyKind, EventKind, RecursiveMode, Watcher};
 use serde::de::DeserializeOwned;
 use serde::Serialize;
@@ -345,12 +346,29 @@ pub async fn plugin_event_loop(
                 for (path, event) in paths {
                     for (plugin_id, watched_path) in file_watch_subs.iter() {
                         if path.starts_with(watched_path) {
-                            let _ = cmd_writer
-                                .send(PluginCommand::HandleUpdate {
-                                    plugin_id: *plugin_id,
-                                    event: event.clone(),
+                            // Use ignore crate to check whether this path would've
+                            // been ignored based on the standard filters.
+                            let walker = WalkBuilder::new(watched_path)
+                                .standard_filters(true)
+                                .build();
+
+                            let valid_paths = walker
+                                .flat_map(|entry| match entry {
+                                    Ok(entry) => Some(entry.into_path()),
+                                    _ => None,
                                 })
-                                .await;
+                                .collect::<HashSet<PathBuf>>();
+
+                            if valid_paths.contains(&path) {
+                                let _ = cmd_writer
+                                    .send(PluginCommand::HandleUpdate {
+                                        plugin_id: *plugin_id,
+                                        event: event.clone(),
+                                    })
+                                    .await;
+                            } else {
+                                log::debug!("ignored changes to {}", path.display());
+                            }
                         }
                     }
                 }
@@ -470,16 +488,6 @@ pub async fn plugin_init(
         .map(|base| base.home_dir().display().to_string())
         .map_or_else(|| "".to_string(), |dir| dir);
 
-    let host_name: String = if let Ok(hname) = hostname::get() {
-        if let Some(hname) = hname.to_str() {
-            hname.to_string()
-        } else {
-            "home.local".to_string()
-        }
-    } else {
-        "home.local".to_string()
-    };
-
     let mut wasi_env = WasiState::new(&plugin.name)
         // Attach the plugin data directory. Anything created by the plugin will live
         // there.
@@ -487,7 +495,6 @@ pub async fn plugin_init(
         .expect("Unable to mount plugin data folder")
         .env(env::BASE_CONFIG_DIR, base_config_dir)
         .env(env::BASE_DATA_DIR, base_data_dir)
-        .env(env::HOST_NAME, host_name)
         .env(env::HOST_HOME_DIR, home_dir)
         .env(env::HOST_OS, std::env::consts::OS)
         // Load user settings as environment variables
