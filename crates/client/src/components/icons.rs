10bebce460094b9fe5725c9bf9db92a3c92c0eac
Andrew Huynh <andrew@spyglass.fyi>
2023-01-25 15:27:53 -0700

    make connections pane match the rest of the settings UI for consistency

--- crates/client/src/components/icons.rs
+++ crates/client/src/components/icons.rs
@@ -374,3 +374,35 @@ pub fn gdrive_logo(props: &IconProps) -> Html {
         </svg>
     }
 }
+
+#[function_component(Gmail)]
+pub fn gmail_logo(props: &IconProps) -> Html {
+    html! {
+        <svg class={props.class()} role="img" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z"/>
+        </svg>
+    }
+}
+
+#[function_component(GitHub)]
+pub fn github_logo(props: &IconProps) -> Html {
+    html! {
+        <svg class={props.class()} fill="currentColor" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/>
+        </svg>
+    }
+}
+
+pub fn connection_icon(id: &str) -> Html {
+    if id == "calendar.google.com" {
+        html! { <GoogleCalendar height="h-6" width="w-6" /> }
+    } else if id == "drive.google.com" {
+        html! { <GDrive height="h-6" width="w-6" /> }
+    } else if id == "mail.google.com" {
+        html! { <Gmail height="h-6" width="w-6" /> }
+    } else if id == "api.github.com" {
+        html! { <GitHub height="h-6" width="w-6" /> }
+    } else {
+        html! { <ShareIcon height="h-6" width="w-6" /> }
+    }
+}
