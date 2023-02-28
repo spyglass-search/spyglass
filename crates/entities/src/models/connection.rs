d7579e28ed87dd9a8214fab9f908299db09474fe
Joel Bredeson <joel@spyglass.fyi>
2023-02-28 12:13:45 -0800

    Update seaorm to 0.11

--- crates/entities/src/models/connection.rs
+++ crates/entities/src/models/connection.rs
@@ -44,9 +44,13 @@ impl RelationTrait for Relation {
     }
 }
 
+#[async_trait::async_trait]
 impl ActiveModelBehavior for ActiveModel {
     // Triggered before insert / update
-    fn before_save(mut self, insert: bool) -> Result<Self, DbErr> {
+    async fn before_save<C>(mut self, _db: &C, insert: bool) -> Result<Self, DbErr>
+    where
+        C: ConnectionTrait,
+    {
         if !insert {
             self.updated_at = Set(chrono::Utc::now());
         }
