3c8c70cffb7b5102f0c5a830c8d2bf8a94e35ae6
Andrew Huynh <andrew@spyglass.fyi>
2023-03-01 12:30:02 -0800

    feature: reddit connection (#348)
    * bumping third-party-api libs to latest & adding reddit lib
    
    * wip: adding reddit sync support
    
    * add author & subreddit tags
    
    * add support for syncing upvoted posts
    
    * add connection resync interval (daily)
    
    * fix workflow
    
    * add missing model in test db creation
    
    * modify spyglass-debug cli to use doc_id or url when fetching document
    
    * fix connection dequeue test
    
    * move dev related Makefile paths into `Makefile.dev` & prompt user to fill that out before continuing
    
    * cleanup dequeue_sync test

--- crates/spyglass/src/connection/credentials.rs
+++ crates/spyglass/src/connection/credentials.rs
@@ -1,5 +1,6 @@
 use libgithub::types::AuthScopes as GithubScope;
 use libgoog::types::AuthScope;
+use libreddit::types::AuthScopes as RedditScope;
 use shared::response::SupportedConnection;
 use std::collections::HashMap;
 
@@ -7,22 +8,24 @@ use std::collections::HashMap;
 pub fn supported_connections() -> HashMap<String, SupportedConnection> {
     let conns = vec![
         SupportedConnection {
-            id: "api.github.com".to_string(),
-            label: "Github".to_string(),
-            description: "Adds indexing support for Github owned repos, starred repos, and issues."
-                .to_string(),
+            id: super::github::API_ID.into(),
+            label: super::github::TITLE.into(),
+            description: super::github::DESCRIPTION.into(),
         },
         SupportedConnection {
-            id: "calendar.google.com".to_string(),
-            label: "Google Calendar".to_string(),
-            description: r#"Adds indexing support for Google calendar events."#.to_string(),
+            id: super::gcal::API_ID.into(),
+            label: super::gcal::TITLE.into(),
+            description: super::gcal::DESCRIPTION.into(),
         },
         SupportedConnection {
-            id: "drive.google.com".to_string(),
-            label: "Google Drive".to_string(),
-            description: r#"Adds indexing support for Google drive. This will allow you
-            to search for through documents, spreadsheets, and presentations."#
-                .to_string(),
+            id: super::gdrive::API_ID.into(),
+            label: super::gdrive::TITLE.into(),
+            description: super::gdrive::DESCRIPTION.into(),
+        },
+        SupportedConnection {
+            id: super::reddit::API_ID.into(),
+            label: super::reddit::TITLE.into(),
+            description: super::reddit::DESCRIPTION.into(),
         },
         // Requires a security audit, lets do this later.
         // SupportedConnection {
@@ -65,6 +68,15 @@ pub fn connection_secret(id: &str) -> Option<(String, String, Vec<String>)> {
             "621713166215-621sdvu6vhj4t03u536p3b2u08o72ndh.apps.googleusercontent.com".to_string(),
             "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".to_string(),
             vec![AuthScope::Gmail.to_string(), AuthScope::Email.to_string()],
+        )),
+        "oauth.reddit.com" => Some((
+            "menLXFXuWDq4y73D1puwAQ".to_string(),
+            "cOHTA8mDH4pAqx9Bz32h1N-i_N2PGw".to_string(),
+            vec![
+                RedditScope::Identity.to_string(),
+                RedditScope::History.to_string(),
+                RedditScope::MySubreddits.to_string(),
+            ],
         )),
         _ => None,
     }
---
+++ crates/spyglass/src/connection/reddit.rs
@@ -0,0 +1,216 @@
+use anyhow::Result;
+use entities::models::{
+    connection,
+    tag::{TagPair, TagType},
+};
+use jsonrpsee::core::async_trait;
+use libreddit::{
+    types::{ApiResponse, Post},
+    RedditClient,
+};
+use strum_macros::{Display, EnumString};
+use url::Url;
+
+use crate::{
+    crawler::{CrawlError, CrawlResult},
+    documents::process_crawl_results,
+    search::Searcher,
+    state::AppState,
+};
+
+use super::{
+    credentials::connection_secret, handle_sync_credentials, load_credentials, Connection,
+};
+
+pub struct RedditConnection {
+    client: RedditClient,
+    user: String,
+}
+
+const BUFFER_SYNC_SIZE: usize = 500;
+
+#[derive(Display, EnumString)]
+enum FavoriteType {
+    #[strum(serialize = "saved")]
+    Saved,
+    #[strum(serialize = "upvoted")]
+    Upvoted,
+}
+
+/// API id for Reddit connections
+pub const API_ID: &str = "oauth.reddit.com";
+/// Lens name for indexed content from Reddit
+pub const LENS: &str = "Reddit";
+pub const TITLE: &str = "Reddit";
+pub const DESCRIPTION: &str = "Adds indexing support for Reddit saved/upvoted posts & comments.";
+
+impl RedditConnection {
+    pub async fn new(state: &AppState, account: &str) -> Result<Self> {
+        let credentials = load_credentials(&state.db, &Self::id(), account).await?;
+        let (client_id, client_secret, _) =
+            connection_secret(&Self::id()).expect("Connection not supported");
+
+        let mut client = RedditClient::new(
+            &client_id,
+            &client_secret,
+            "http://127.0.0.1:0",
+            credentials,
+        )?;
+
+        handle_sync_credentials(&mut client, &state.db, &Self::id(), account).await;
+        Ok(Self {
+            client,
+            user: account.to_string(),
+        })
+    }
+
+    pub fn to_url(&self, id: &str) -> Result<Url> {
+        let mut url_base = Url::parse("api://oauth.reddit.com/api/info")?;
+        url_base.set_query(Some(&format!("id={}", id)));
+        let _ = url_base.set_username(&self.user);
+        Ok(url_base)
+    }
+
+    async fn process_list(
+        &mut self,
+        state: &AppState,
+        buffer: &mut Vec<Post>,
+        resp: &ApiResponse<Vec<Post>>,
+        tags: &[TagPair],
+    ) -> Option<String> {
+        let page = resp.after.clone();
+        buffer.extend(resp.data.clone());
+
+        if buffer.len() > BUFFER_SYNC_SIZE || page.is_none() {
+            // Add to database
+            let mut posts = Vec::new();
+            for post in buffer.iter() {
+                let api_url = self.to_url(&post.name).expect("unable to create url");
+                log::debug!("post: {}", api_url);
+                posts.push(post_to_crawl(&api_url, post));
+            }
+
+            if !posts.is_empty() {
+                if let Err(err) = process_crawl_results(state, &posts, tags).await {
+                    log::warn!("Unable to add posts: {}", err);
+                }
+
+                if let Err(err) = Searcher::save(state).await {
+                    log::warn!("Unable to save posts: {}", err);
+                }
+            }
+
+            buffer.clear();
+        }
+
+        page
+    }
+
+    async fn sync_saved(&mut self, state: &AppState) -> Result<usize> {
+        let mut page = None;
+        let mut total_synced = 0;
+        let mut buffer = Vec::new();
+
+        let mut tags = self.default_tags().clone();
+        tags.push((TagType::Favorited, FavoriteType::Saved.to_string()));
+
+        loop {
+            let resp = self.client.list_saved(page, 100).await?;
+            total_synced += resp.data.len();
+            page = self.process_list(state, &mut buffer, &resp, &tags).await;
+
+            if page.is_none() {
+                break;
+            }
+        }
+
+        Ok(total_synced)
+    }
+
+    async fn sync_upvoted(&mut self, state: &AppState) -> Result<usize> {
+        let mut page = None;
+        let mut total_synced = 0;
+        let mut buffer = Vec::new();
+
+        let mut tags = self.default_tags().clone();
+        tags.push((TagType::Favorited, FavoriteType::Upvoted.to_string()));
+
+        loop {
+            let resp = self.client.list_upvoted(page, 100).await?;
+            total_synced += resp.data.len();
+            page = self.process_list(state, &mut buffer, &resp, &tags).await;
+
+            if page.is_none() {
+                break;
+            }
+        }
+
+        Ok(total_synced)
+    }
+}
+
+#[async_trait]
+impl Connection for RedditConnection {
+    fn id() -> String {
+        API_ID.to_string()
+    }
+
+    fn user(&self) -> String {
+        self.user.clone()
+    }
+
+    fn default_tags(&self) -> Vec<TagPair> {
+        vec![(TagType::Source, Self::id()), (TagType::Lens, LENS.into())]
+    }
+
+    async fn sync(&mut self, state: &AppState) {
+        log::debug!("syncing w/ connection: {}", &Self::id());
+        let _ = connection::set_sync_status(&state.db, &Self::id(), &self.user, true).await;
+
+        match self.sync_saved(state).await {
+            Ok(num_synced) => log::info!("synced {num_synced} saved posts"),
+            Err(err) => log::warn!("Unable to sync saved posts: {err}"),
+        }
+
+        match self.sync_upvoted(state).await {
+            Ok(num_synced) => log::info!("synced {num_synced} upvoted posts"),
+            Err(err) => log::warn!("Unable to sync upvoted posts: {}", err),
+        }
+
+        let _ = connection::set_sync_status(&state.db, &Self::id(), &self.user, false).await;
+    }
+
+    async fn get(&mut self, _: &Url) -> anyhow::Result<CrawlResult, CrawlError> {
+        Err(CrawlError::Other("not supported".into()))
+    }
+}
+
+fn post_to_crawl(api_url: &Url, post: &Post) -> CrawlResult {
+    let open_url = format!("https://www.reddit.com{}", post.permalink);
+    let mut tags: Vec<TagPair> = Vec::new();
+
+    tags.push((TagType::Owner, post.author.clone()));
+    tags.push((TagType::Other("subreddit".into()), post.subreddit.clone()));
+
+    // Comment
+    let mut result =
+        if let (Some(title), Some(body)) = (post.link_title.as_deref(), post.body.as_deref()) {
+            tags.push((TagType::Type, "Comment".into()));
+            CrawlResult::new(api_url, Some(open_url), body, title, None)
+        } else {
+            let content = if post.is_self {
+                tags.push((TagType::Type, "Post".into()));
+                post.selftext.clone()
+            } else {
+                tags.push((TagType::Type, "Link".into()));
+                post.url.clone()
+            };
+
+            let title = post.title.as_deref().unwrap_or_default();
+
+            CrawlResult::new(api_url, Some(open_url), &content, title, None)
+        };
+
+    result.tags.extend(tags);
+    result
+}
