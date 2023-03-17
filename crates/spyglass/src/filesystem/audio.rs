8c14e3a4566630b149fe49a92b5ff05bc486b0b9
Andrew Huynh <andrew@spyglass.fyi>
2023-03-17 14:25:40 -0700

    improved error handling

--- crates/spyglass/src/filesystem/audio.rs
+++ crates/spyglass/src/filesystem/audio.rs
@@ -1,4 +1,8 @@
-use rubato::{InterpolationParameters, InterpolationType, Resampler, SincFixedIn, WindowFunction};
+use anyhow::anyhow;
+use rubato::{
+    InterpolationParameters, InterpolationType, Resampler, ResamplerConstructionError, SincFixedIn,
+    WindowFunction,
+};
 use std::path::PathBuf;
 use symphonia::core::{
     audio::SampleBuffer,
@@ -13,7 +17,7 @@ use whisper_rs::{FullParams, SamplingStrategy, WhisperContext};
 
 const MODEL_PATH: &str = "../../assets/models/whisper.base.en.bin";
 
-fn resample(og: &[f32], og_rate: u32) -> Vec<f32> {
+fn resample(og: &[f32], og_rate: u32) -> Result<Vec<f32>, ResamplerConstructionError> {
     let params = InterpolationParameters {
         sinc_len: 256,
         f_cutoff: 0.95,
@@ -23,20 +27,19 @@ fn resample(og: &[f32], og_rate: u32) -> Vec<f32> {
     };
 
     let mut resampler =
-        SincFixedIn::<f32>::new(16_000f64 / og_rate as f64, 2.0, params, og.len(), 1)
-            .expect("Unable to create resampler");
+        SincFixedIn::<f32>::new(16_000f64 / og_rate as f64, 2.0, params, og.len(), 1)?;
 
     let waves_in = vec![og.to_vec()];
     let mut waves_out = resampler.process(&waves_in, None).unwrap_or_default();
     if waves_out.is_empty() {
-        Vec::new()
+        Ok(Vec::new())
     } else {
-        waves_out.pop().unwrap_or_default()
+        Ok(waves_out.pop().unwrap_or_default())
     }
 }
 
 // todo: handling streaming in large files
-fn parse_audio_file(path: &PathBuf) -> Vec<f32> {
+fn parse_audio_file(path: &PathBuf) -> anyhow::Result<Vec<f32>> {
     let src = std::fs::File::open(path).expect("Unable open media");
     let mss = MediaSourceStream::new(Box::new(src), Default::default());
 
@@ -51,9 +54,7 @@ fn parse_audio_file(path: &PathBuf) -> Vec<f32> {
     let fmt_opts: FormatOptions = Default::default();
 
     // Probe the media source.
-    let probed = symphonia::default::get_probe()
-        .format(&hint, mss, &fmt_opts, &meta_opts)
-        .expect("unsupported format");
+    let probed = symphonia::default::get_probe().format(&hint, mss, &fmt_opts, &meta_opts)?;
 
     // Get the instantiated format reader.
     let mut format = probed.format;
@@ -62,20 +63,22 @@ fn parse_audio_file(path: &PathBuf) -> Vec<f32> {
     let track = format
         .tracks()
         .iter()
-        .find(|t| t.codec_params.codec != CODEC_TYPE_NULL)
-        .expect("no supported audio tracks");
+        .find(|t| t.codec_params.codec != CODEC_TYPE_NULL);
+
+    let track = match track {
+        Some(track) => track,
+        None => return Err(anyhow!("Unable to find valid track")),
+    };
 
     // Use the default options for the decoder.
     let dec_opts: DecoderOptions = Default::default();
-    let sample_rate = track
-        .codec_params
-        .sample_rate
-        .expect("No sample rate detected");
+    let sample_rate = track.codec_params.sample_rate.unwrap_or_default();
+    if sample_rate == 0 {
+        return Err(anyhow!("Invalid sample rate"));
+    }
 
     // Create a decoder for the track.
-    let mut decoder = symphonia::default::get_codecs()
-        .make(&track.codec_params, &dec_opts)
-        .expect("unsupported codec");
+    let mut decoder = symphonia::default::get_codecs().make(&track.codec_params, &dec_opts)?;
 
     // Store the track identifier, it will be used to filter packets.
     let track_id = track.id;
@@ -120,7 +123,7 @@ fn parse_audio_file(path: &PathBuf) -> Vec<f32> {
         }
     }
 
-    println!(
+    log::debug!(
         "decoded {} samples, buf len: {}",
         sample_count,
         samples.len()
@@ -128,11 +131,15 @@ fn parse_audio_file(path: &PathBuf) -> Vec<f32> {
 
     // Do we need to resample?
     if sample_rate != 16_000 {
-        println!("resampling from {} to 16000", sample_rate);
-        samples = resample(&samples, sample_rate);
+        log::debug!("resampling from {} to 16000", sample_rate);
+        if let Ok(new_samples) = resample(&samples, sample_rate) {
+            samples = new_samples;
+        } else {
+            log::warn!("failed to resample, failing back to original samples");
+        }
     }
 
-    samples
+    Ok(samples)
 }
 
 #[derive(Clone, Debug)]
@@ -153,33 +160,33 @@ impl Segment {
 }
 
 /// Given a path to a wav file, transcribe it using our **shhhh** models.
-fn transcibe_audio(path: PathBuf, segment_len: i32) -> Vec<Segment> {
+fn transcibe_audio(path: PathBuf, segment_len: i32) -> anyhow::Result<Vec<Segment>> {
     if !path.exists() || !path.is_file() {
-        dbg!(path.exists(), path.is_file());
-        panic!("expected a file");
+        return Err(anyhow!("Invalid file path"));
     }
 
     let mut segments = Vec::new();
-    let samples = parse_audio_file(&path);
-    let mut ctx = WhisperContext::new(MODEL_PATH).expect("failed to open model");
+    if let Ok(samples) = parse_audio_file(&path) {
+        let mut ctx = WhisperContext::new(MODEL_PATH).expect("failed to open model");
 
-    let mut params = FullParams::new(SamplingStrategy::default());
-    params.set_max_len(segment_len);
-    params.set_print_progress(false);
-    params.set_token_timestamps(true);
+        let mut params = FullParams::new(SamplingStrategy::default());
+        params.set_max_len(segment_len);
+        params.set_print_progress(false);
+        params.set_token_timestamps(true);
 
-    ctx.full(params, &samples)
-        .expect("failed to convert samples");
-    let num_segments = ctx.full_n_segments();
-    println!("Extracted {} segments", num_segments);
-    for i in 0..num_segments {
-        let segment = ctx.full_get_segment_text(i).expect("failed to get segment");
-        let start_timestamp = ctx.full_get_segment_t0(i);
-        let end_timestamp = ctx.full_get_segment_t1(i);
-        segments.push(Segment::new(start_timestamp, end_timestamp, &segment));
+        ctx.full(params, &samples)
+            .expect("failed to convert samples");
+        let num_segments = ctx.full_n_segments();
+        println!("Extracted {} segments", num_segments);
+        for i in 0..num_segments {
+            let segment = ctx.full_get_segment_text(i).expect("failed to get segment");
+            let start_timestamp = ctx.full_get_segment_t0(i);
+            let end_timestamp = ctx.full_get_segment_t1(i);
+            segments.push(Segment::new(start_timestamp, end_timestamp, &segment));
+        }
     }
 
-    segments
+    Ok(segments)
 }
 
 #[cfg(test)]
@@ -191,7 +198,7 @@ mod test {
     fn test_wav_transcription() {
         let expected = include_str!("../../../../fixtures/audio/jfk.txt");
         let path = "../../fixtures/audio/jfk.wav".into();
-        let segments = transcibe_audio(path, 1);
+        let segments = transcibe_audio(path, 1).expect("Unable to transcribe");
         assert!(segments.len() > 0);
 
         let combined = segments
@@ -207,7 +214,7 @@ mod test {
     fn test_ogg_transcription() {
         let expected = include_str!("../../../../fixtures/audio/armstrong.txt");
         let path = "../../fixtures/audio/armstrong.ogg".into();
-        let segments = transcibe_audio(path, 1);
+        let segments = transcibe_audio(path, 1).expect("Unable to transcribe");
         assert!(segments.len() > 0);
         let combined = segments
             .iter()
