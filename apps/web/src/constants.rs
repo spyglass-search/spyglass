0972476ce4bdb6f56e3bfc257c3830ab51d778b1
Andrew Huynh <andrew@spyglass.fyi>
2023-04-17 10:54:30 -0700

    add dev/prod endpoints for web client

--- apps/web/src/constants.rs
+++ apps/web/src/constants.rs
@@ -1,3 +1,7 @@
 // todo: pull these from environment variables? config?
+#[cfg(not(debug_assertions))]
 pub const HTTP_ENDPOINT: &str = "https://search.spyglass.fyi";
+#[cfg(debug_assertions)]
+pub const HTTP_ENDPOINT: &str = "http://127.0.0.1:8757";
+
 pub const RPC_ENDPOINT: &str = "http://127.0.0.1:4664";
