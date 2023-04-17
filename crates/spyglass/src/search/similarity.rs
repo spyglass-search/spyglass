89a1260928394ec3e51bc6ed09900d95da767f0d
Andrew Huynh <andrew@spyglass.fyi>
2023-04-17 11:13:35 -0700

    similarity_search: separate log & json timings

--- crates/spyglass/src/search/similarity.rs
+++ crates/spyglass/src/search/similarity.rs
@@ -6,9 +6,9 @@ pub async fn similarity_search(query: &str) -> Vec<SimilaritySearchResult> {
     let bench_start = SystemTime::now();
     let endpoint = env::var("SIMILARITY_SEARCH_ENDPOINT")
         .unwrap_or("localhost".into());
-
     let port = env::var("SIMILARITY_SEARCH_PORT")
         .unwrap_or("8000".into());
+    log::info!("using {}:{}", endpoint, port);
     log::info!(
         "env_check: {}ms",
         bench_start.elapsed().unwrap().as_millis()
@@ -24,17 +24,21 @@ pub async fn similarity_search(query: &str) -> Vec<SimilaritySearchResult> {
 
     // todo: pull endpoint from environment / configuration
     let bench_start = SystemTime::now();
-    let results: Vec<SimilaritySearchResult> = client
+    let resp = client
         .post(format!("http://{}:{}/search", endpoint, port))
         .json(&serde_json::json!({ "query": query }))
         .send()
         .await
-        .unwrap()
-        .json()
-        .await
-        .unwrap_or_default();
+        .unwrap();
     log::info!(
-        "search_call: {}ms",
+        "similarity_search_call: {}ms",
+        bench_start.elapsed().unwrap().as_millis()
+    );
+
+    let bench_start = SystemTime::now();
+    let results = resp.json().await.unwrap_or_default();
+    log::info!(
+        "similarity_json: {}ms",
         bench_start.elapsed().unwrap().as_millis()
     );
 
