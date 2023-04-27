edfb84da406b3ed0a10bd1725df1923e2c53babf
Andrew Huynh <andrew@spyglass.fyi>
2023-04-26 20:35:14 -0700

    update endpoints for web client

--- apps/web/src/constants.rs
+++ apps/web/src/constants.rs
@@ -1,5 +1,5 @@
 // todo: pull these from environment variables? config?
 #[cfg(not(debug_assertions))]
-pub const HTTP_ENDPOINT: &str = "https://search.spyglass.fyi";
+pub const HTTP_ENDPOINT: &str = "https://api.spyglass.fyi";
 #[cfg(debug_assertions)]
 pub const HTTP_ENDPOINT: &str = "http://127.0.0.1:8757";
