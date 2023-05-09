867936e7fe16f76a08f90acb96bb42c0c77ae81c
Renato Dinhani <renatodinhani@gmail.com>
2023-05-09 13:44:20 -0300

    feat: extracting pdf title and author from metadata and showing them in the search (#447)

--- crates/spyglass/src/parser/mod.rs
+++ crates/spyglass/src/parser/mod.rs
@@ -8,17 +8,40 @@ pub mod xlsx_parser;
 /*
  * Parses the specified file
  */
-pub fn parse_file(extension: &OsStr, file_path: &Path) -> anyhow::Result<String> {
+pub fn parse_file(extension: &OsStr, file_path: &Path) -> anyhow::Result<ParsedDocument> {
     if extension.eq_ignore_ascii_case("docx") {
-        return docx_parser::parse(file_path);
+        Ok(ParsedDocument {
+            content: docx_parser::parse(file_path)?,
+            ..Default::default()
+        })
     } else if extension.eq_ignore_ascii_case("xlsx")
         || extension.eq_ignore_ascii_case("xls")
         || extension.eq_ignore_ascii_case("ods")
     {
-        return xlsx_parser::parse(file_path);
+        Ok(ParsedDocument {
+            content: xlsx_parser::parse(file_path)?,
+            ..Default::default()
+        })
     } else if extension.eq_ignore_ascii_case("pdf") {
-        return pdf_parser::parse(file_path);
+        Ok(pdf_parser::parse(file_path)?.into())
+    } else {
+        Err(anyhow!(format!("Extension {extension:?} not supported")))
     }
+}
 
-    Err(anyhow!(format!("Extension {extension:?} not supported")))
+#[derive(Default)]
+pub struct ParsedDocument {
+    pub title: Option<String>,
+    pub author: Option<String>,
+    pub content: String,
+}
+
+impl From<pdf_parser::Pdf> for ParsedDocument {
+    fn from(value: pdf_parser::Pdf) -> Self {
+        Self {
+            title: value.metadata.title,
+            author: value.metadata.author,
+            content: value.content,
+        }
+    }
 }
