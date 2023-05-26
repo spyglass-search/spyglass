69d73a0200ec3b882354236600974fe3d56c77ad
Andrew Huynh <andrew@spyglass.fyi>
2023-05-25 18:28:37 -0700

    continuing to move more stuff over to add_source component

--- apps/web/src/pages/lens_editor/add_source.rs
+++ apps/web/src/pages/lens_editor/add_source.rs
@@ -1,44 +1,326 @@
-use ui_components::btn::Btn;
-use yew::prelude::*;
+use strum::IntoEnumIterator;
+use strum_macros::{Display, EnumIter};
+use ui_components::btn::{Btn, BtnSize, BtnType};
+use wasm_bindgen::{prelude::*, JsValue};
+use web_sys::HtmlInputElement;
+use yew::{html::Scope, platform::spawn_local, prelude::*};
+
+use crate::{
+    client::{ApiClient, ApiError, LensAddDocType, LensAddDocument},
+    AuthStatus,
+};
+
+#[wasm_bindgen(module = "/public/gapi.js")]
+extern "C" {
+    #[wasm_bindgen]
+    pub fn init_gapi(client_id: &str, api_key: &str);
+
+    #[wasm_bindgen(catch)]
+    pub async fn create_picker(cb: &Closure<dyn Fn(JsValue, JsValue)>) -> Result<(), JsValue>;
+}
+
+#[derive(Clone, EnumIter, Display, PartialEq, Eq)]
+pub enum AddSourceTabs {
+    Website,
+    Podcast,
+    GDrive,
+}
 
 pub struct AddSourceComponent {
-    error_msg: Option<String>
+    auth_status: AuthStatus,
+    error_msg: Option<String>,
+    selected_tab: AddSourceTabs,
+    _context_listener: ContextHandle<AuthStatus>,
+    _url_input_ref: NodeRef,
+    _url_crawl_ref: NodeRef,
 }
 
 pub enum Msg {
     AddUrl { include_all: bool },
+    ChangeToTab(AddSourceTabs),
+    EmitUpdate,
+    FilePicked { token: String, url: String },
+    SetError(String),
     OpenCloudFilePicker,
+    UpdateContext(AuthStatus),
+}
+
+#[derive(Properties, PartialEq)]
+pub struct AddSourceComponentProps {
+    pub lens_identifier: String,
+    #[prop_or_default]
+    pub on_update: Callback<()>,
 }
 
 impl Component for AddSourceComponent {
     type Message = Msg;
-    type Properties = ();
+    type Properties = AddSourceComponentProps;
 
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self { error_msg: None }
+    fn create(ctx: &Context<Self>) -> Self {
+        // initialize gapi
+        init_gapi(dotenv!("GOOGLE_CLIENT_ID"), dotenv!("GOOGLE_API_KEY"));
+        // Connect to context for auth details
+        let (auth_status, context_listener) = ctx
+            .link()
+            .context(ctx.link().callback(Msg::UpdateContext))
+            .expect("No Message Context Provided");
+
+        Self {
+            auth_status,
+            error_msg: None,
+            selected_tab: AddSourceTabs::Website,
+            _context_listener: context_listener,
+            _url_input_ref: NodeRef::default(),
+            _url_crawl_ref: NodeRef::default(),
+        }
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
-        true
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        let link = ctx.link();
+        let props = ctx.props();
+
+        match msg {
+            Msg::AddUrl { include_all } => {
+                if let Some(node) = self._url_input_ref.cast::<HtmlInputElement>() {
+                    let url = node.value();
+
+                    if let Err(_err) = url::Url::parse(&url) {
+                        link.send_message(Msg::SetError("Invalid Url".to_string()));
+                    } else {
+                        let new_source = LensAddDocument {
+                            url,
+                            doc_type: LensAddDocType::WebUrl {
+                                include_all_suburls: include_all,
+                            },
+                        };
+                        // Add to lens
+                        let auth_status = self.auth_status.clone();
+                        let identifier = props.lens_identifier.clone();
+                        let link = link.clone();
+
+                        if include_all {
+                            spawn_local(async move {
+                                let api = auth_status.get_client();
+                                match api.validate_lens_source(&identifier, &new_source).await {
+                                    Ok(response) => {
+                                        if response.is_valid {
+                                            node.set_value("");
+                                            add_lens_source(&api, &new_source, &identifier, link)
+                                                .await;
+                                        } else if let Some(error_msg) = response.validation_msg {
+                                            link.send_message(Msg::SetError(error_msg));
+                                        } else {
+                                            link.send_message(Msg::SetError(
+                                                "Unknown error adding url".to_string(),
+                                            ));
+                                        }
+                                    }
+                                    Err(error) => {
+                                        log::error!("Unknown error adding url {:?}", error);
+                                        link.send_message(Msg::SetError(
+                                            "Unknown error adding url".to_string(),
+                                        ));
+                                    }
+                                }
+                            })
+                        } else {
+                            node.set_value("");
+                            spawn_local(async move {
+                                let api = auth_status.get_client();
+                                add_lens_source(&api, &new_source, &identifier, link).await;
+                            });
+                        }
+                    }
+                }
+                true
+            }
+            Msg::ChangeToTab(new_tab) => {
+                self.selected_tab = new_tab;
+                true
+            }
+            Msg::EmitUpdate => {
+                props.on_update.emit(());
+                false
+            }
+            Msg::FilePicked { token, url } => {
+                let new_source = LensAddDocument {
+                    url,
+                    doc_type: LensAddDocType::GDrive { token },
+                };
+
+                // Add to lens
+                let auth_status = self.auth_status.clone();
+                let identifier = props.lens_identifier.clone();
+                let link = link.clone();
+                spawn_local(async move {
+                    let api = auth_status.get_client();
+                    if let Err(err) = api.lens_add_source(&identifier, &new_source).await {
+                        log::error!("error adding gdrive source: {err}");
+                    } else {
+                        link.send_message(Msg::EmitUpdate);
+                    }
+                });
+                true
+            }
+            Msg::OpenCloudFilePicker => {
+                let link = link.clone();
+                spawn_local(async move {
+                    let cb = Closure::wrap(Box::new(move |token: JsValue, payload: JsValue| {
+                        if let (Ok(token), Ok(url)) = (
+                            serde_wasm_bindgen::from_value::<String>(token),
+                            serde_wasm_bindgen::from_value::<String>(payload),
+                        ) {
+                            link.send_message(Msg::FilePicked { token, url });
+                        }
+                    }) as Box<dyn Fn(JsValue, JsValue)>);
+
+                    if let Err(err) = create_picker(&cb).await {
+                        log::error!("create_picker error: {:?}", err);
+                    }
+                    cb.forget();
+                });
+                false
+            }
+            Msg::UpdateContext(auth_status) => {
+                self.auth_status = auth_status;
+                true
+            }
+            _ => false,
+        }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         let link = ctx.link();
 
+        let tab_styles = classes!(
+            "border-b-2",
+            "px-4",
+            "pb-2",
+            "-mb-[2px]",
+            "cursor-pointer",
+            "hover:text-cyan-500",
+            "hover:border-cyan-500"
+        );
+        let tabs = AddSourceTabs::iter()
+            .map(|tab| {
+                let mut styles = tab_styles.clone();
+                if self.selected_tab == tab {
+                    styles.extend(vec!["text-cyan-500", "border-cyan-500"]);
+                } else {
+                    styles.extend(vec!["text-white", "border-neutral-700"]);
+                }
+
+                let action = {
+                    let new_tab = tab.clone();
+                    link.callback(move |_| Msg::ChangeToTab(new_tab.clone()))
+                };
+
+                html! { <li onclick={action} class={styles.clone()}>{tab}</li> }
+            })
+            .collect::<Html>();
+
         html! {
             <div class="flex flex-col gap-4">
-                <div class="flex flex-row gap-4 items-center">
-                    <input
-                        type="text"
-                        class="rounded p-2 text-sm text-neutral-800"
-                        placeholder="https://example.com"
-                    />
-                    <Btn onclick={link.callback(|_| Msg::AddUrl {include_all: false})}>{"Add data from URL"}</Btn>
-                    <Btn onclick={link.callback(|_| Msg::AddUrl {include_all: true} )}>{"Add all URLs from Site"}</Btn>
-                    <div class="text-sm text-red-700">{self.error_msg.clone()}</div>
+                {if let Some(msg) = &self.error_msg {
+                    html! {
+                        <div class="bg-red-300 border border-red-700 text-red-700 rounded-lg text-sm p-2 font-bold">
+                            {msg}
+                        </div>
+                    }
+                } else { html! {} }}
+                <div>
+                    <ul class="text-sm flex flex-row gap-8 w-full border-b-2 border-neutral-700 mb-4">
+                        {tabs}
+                    </ul>
+                    <div class="px-2 md:px-8 py-2">
+                        {match self.selected_tab {
+                            AddSourceTabs::Website => self.view_website_tab(link),
+                            AddSourceTabs::Podcast => self.view_podcast_tab(link),
+                            AddSourceTabs::GDrive => self.view_gdrive_tab(link),
+                        }}
+                    </div>
                 </div>
-                <div><Btn onclick={link.callback(|_| Msg::OpenCloudFilePicker)}>{"Add data from Google Drive"}</Btn></div>
             </div>
         }
+    }
+}
+
+impl AddSourceComponent {
+    fn view_website_tab(&self, link: &Scope<AddSourceComponent>) -> Html {
+        html! {
+            <div>
+                <div class="text-xs text-neutral-400 pb-2">
+                    {"Add a single page or all pages from a website"}
+                </div>
+                <div class="flex flex-row gap-4 items-center">
+                    <input
+                        ref={self._url_input_ref.clone()}
+                        type="text"
+                        class="rounded p-2 text-sm text-neutral-800 flex-grow"
+                        placeholder="https://example.com"
+                    />
+                    <div>
+                        <label class="flex flex-row gap-2 text-sm">
+                            <input
+                                ref={self._url_crawl_ref.clone()}
+                                type="checkbox"
+                            />
+                            {"Add all pages"}
+                        </label>
+                    </div>
+                    <Btn size={BtnSize::Sm} _type={BtnType::Primary} onclick={link.callback(|_| Msg::AddUrl {include_all: false})}>
+                        {"Fetch"}
+                    </Btn>
+                </div>
+            </div>
+        }
+    }
+
+    fn view_podcast_tab(&self, link: &Scope<AddSourceComponent>) -> Html {
+        html! {
+            <div>
+                <div class="text-xs text-neutral-400 pb-2">
+                    {"Add episodes from a podcast feed"}
+                </div>
+                <div class="flex flex-row gap-4 items-center">
+                    <input
+                        ref={self._url_input_ref.clone()}
+                        type="text"
+                        class="rounded p-2 text-sm text-neutral-800 flex-grow"
+                        placeholder="https://example.com/feed.rss"
+                    />
+                    <Btn size={BtnSize::Sm} _type={BtnType::Primary} onclick={link.callback(|_| Msg::AddUrl {include_all: false})}>
+                        {"Add Podcast"}
+                    </Btn>
+                </div>
+            </div>
+        }
+    }
+
+    fn view_gdrive_tab(&self, link: &Scope<AddSourceComponent>) -> Html {
+        html! {
+            <div>
+                <Btn onclick={link.callback(|_| Msg::OpenCloudFilePicker)} _type={BtnType::Primary}>
+                    {"Select file from Google Drive"}
+                </Btn>
+            </div>
+        }
+    }
+}
+
+async fn add_lens_source(
+    api: &ApiClient,
+    new_source: &LensAddDocument,
+    identifier: &str,
+    link: Scope<AddSourceComponent>,
+) {
+    if let Err(err) = api.lens_add_source(identifier, new_source).await {
+        log::error!("error adding url source: {err}");
+        match err {
+            ApiError::ClientError(msg) => link.send_message(Msg::SetError(msg.message)),
+            _ => link.send_message(Msg::SetError(err.to_string())),
+        };
+    } else {
+        link.send_message(Msg::EmitUpdate);
     }
 }
--- apps/web/src/pages/lens_editor/mod.rs
+++ apps/web/src/pages/lens_editor/mod.rs
@@ -4,21 +4,14 @@ use ui_components::{
     icons,
     results::Paginator,
 };
-use wasm_bindgen::{
-    prelude::{wasm_bindgen, Closure},
-    JsValue,
-};
+use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 use wasm_bindgen_futures::spawn_local;
 use web_sys::HtmlInputElement;
-use yew::html::Scope;
 use yew::prelude::*;
 use yew_router::scope_ext::RouterScopeExt;
 
 use crate::{
-    client::{
-        ApiClient, ApiError, GetLensSourceResponse, Lens, LensAddDocType, LensAddDocument,
-        LensDocType, LensSource,
-    },
+    client::{ApiError, GetLensSourceResponse, Lens, LensDocType, LensSource},
     AuthStatus,
 };
 
@@ -26,15 +19,6 @@ mod add_source;
 use add_source::AddSourceComponent;
 
 const QUERY_DEBOUNCE_MS: u32 = 1_000;
-
-#[wasm_bindgen(module = "/public/gapi.js")]
-extern "C" {
-    #[wasm_bindgen]
-    pub fn init_gapi(client_id: &str, api_key: &str);
-
-    #[wasm_bindgen(catch)]
-    pub async fn create_picker(cb: &Closure<dyn Fn(JsValue, JsValue)>) -> Result<(), JsValue>;
-}
 
 #[wasm_bindgen]
 extern "C" {
@@ -59,11 +43,9 @@ pub struct CreateLensPage {
 
     pub auth_status: AuthStatus,
     pub add_url_error: Option<String>,
-    pub processing_action: Option<Action>,
     pub _context_listener: ContextHandle<AuthStatus>,
     pub _query_debounce: Option<JsValue>,
     pub _name_input_ref: NodeRef,
-    pub _url_input_ref: NodeRef,
 }
 
 #[derive(Properties, PartialEq)]
@@ -71,24 +53,13 @@ pub struct CreateLensProps {
     pub lens: String,
 }
 
-pub enum Action {
-    AddSingleUrl,
-    AddAllUrls,
-}
-
 pub enum Msg {
-    AddUrl { include_all: bool },
-    AddUrlError(String),
-    ClearUrlError,
-    Processing(Option<Action>),
     DeleteLensSource(LensSource),
-    FilePicked { token: String, url: String },
     Reload,
     ReloadSources(usize),
     Save { display_name: String },
     SetLensData(Lens),
     SetLensSources(GetLensSourceResponse),
-    OpenCloudFilePicker,
     UpdateContext(AuthStatus),
     UpdateDisplayName,
 }
@@ -98,9 +69,6 @@ impl Component for CreateLensPage {
     type Properties = CreateLensProps;
 
     fn create(ctx: &Context<Self>) -> Self {
-        // initialize gapi
-        init_gapi(dotenv!("GOOGLE_CLIENT_ID"), dotenv!("GOOGLE_API_KEY"));
-
         let (auth_status, context_listener) = ctx
             .link()
             .context(ctx.link().callback(Msg::UpdateContext))
@@ -117,11 +85,9 @@ impl Component for CreateLensPage {
             is_loading_lens_sources: false,
             auth_status,
             add_url_error: None,
-            processing_action: None,
             _context_listener: context_listener,
             _query_debounce: None,
             _name_input_ref: NodeRef::default(),
-            _url_input_ref: NodeRef::default(),
         }
     }
 
@@ -147,83 +113,6 @@ impl Component for CreateLensPage {
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         let link = ctx.link();
         match msg {
-            Msg::AddUrl { include_all } => {
-                if let Some(node) = self._url_input_ref.cast::<HtmlInputElement>() {
-                    let url = node.value();
-
-                    if let Err(_err) = url::Url::parse(&url) {
-                        link.send_message(Msg::AddUrlError("Invalid Url".to_string()));
-                    } else {
-                        let new_source = LensAddDocument {
-                            url,
-                            doc_type: LensAddDocType::WebUrl {
-                                include_all_suburls: include_all,
-                            },
-                        };
-                        // Add to lens
-                        let auth_status = self.auth_status.clone();
-                        let identifier = self.lens_identifier.clone();
-                        let link = link.clone();
-
-                        if include_all {
-                            spawn_local(async move {
-                                link.send_message(Msg::Processing(Some(Action::AddAllUrls)));
-                                let api = auth_status.get_client();
-                                match api.validate_lens_source(&identifier, &new_source).await {
-                                    Ok(response) => {
-                                        if response.is_valid {
-                                            node.set_value("");
-                                            add_lens_source(&api, &new_source, &identifier, link)
-                                                .await;
-                                        } else if let Some(error_msg) = response.validation_msg {
-                                            link.send_message_batch(vec![
-                                                Msg::Processing(None),
-                                                Msg::AddUrlError(error_msg),
-                                            ])
-                                        } else {
-                                            link.send_message_batch(vec![
-                                                Msg::Processing(None),
-                                                Msg::AddUrlError(
-                                                    "Unknown error adding url".to_string(),
-                                                ),
-                                            ])
-                                        }
-                                    }
-                                    Err(error) => {
-                                        log::error!("Unknown error adding url {:?}", error);
-                                        link.send_message_batch(vec![
-                                            Msg::Processing(None),
-                                            Msg::AddUrlError(
-                                                "Unknown error adding url".to_string(),
-                                            ),
-                                        ])
-                                    }
-                                }
-                            })
-                        } else {
-                            node.set_value("");
-                            spawn_local(async move {
-                                link.send_message(Msg::Processing(Some(Action::AddSingleUrl)));
-                                let api = auth_status.get_client();
-                                add_lens_source(&api, &new_source, &identifier, link).await;
-                            });
-                        }
-                    }
-                }
-                true
-            }
-            Msg::AddUrlError(msg) => {
-                self.add_url_error = Some(msg);
-                true
-            }
-            Msg::ClearUrlError => {
-                self.add_url_error = None;
-                true
-            }
-            Msg::Processing(action) => {
-                self.processing_action = action;
-                true
-            }
             Msg::DeleteLensSource(source) => {
                 // Add to lens
                 let auth_status = self.auth_status.clone();
@@ -235,7 +124,6 @@ impl Component for CreateLensPage {
                     .map(|x| x.page)
                     .unwrap_or(0);
                 spawn_local(async move {
-                    link.send_message(Msg::Processing(Some(Action::AddAllUrls)));
                     let api = auth_status.get_client();
                     if let Err(error) = api.delete_lens_source(&identifier, &source.doc_uuid).await
                     {
@@ -246,27 +134,6 @@ impl Component for CreateLensPage {
                     }
                 });
                 false
-            }
-            Msg::FilePicked { token, url } => {
-                let new_source = LensAddDocument {
-                    url,
-                    doc_type: LensAddDocType::GDrive { token },
-                };
-
-                // Add to lens
-                let auth_status = self.auth_status.clone();
-                let identifier = self.lens_identifier.clone();
-                let link = link.clone();
-                spawn_local(async move {
-                    let api = auth_status.get_client();
-                    if let Err(err) = api.lens_add_source(&identifier, &new_source).await {
-                        log::error!("error adding gdrive source: {err}");
-                    } else {
-                        // Reload data if successful
-                        link.send_message(Msg::Reload);
-                    }
-                });
-                true
             }
             Msg::Reload => {
                 let auth_status = self.auth_status.clone();
@@ -342,25 +209,6 @@ impl Component for CreateLensPage {
                 self.lens_sources = Some(sources.results);
                 true
             }
-            Msg::OpenCloudFilePicker => {
-                let link = link.clone();
-                spawn_local(async move {
-                    let cb = Closure::wrap(Box::new(move |token: JsValue, payload: JsValue| {
-                        if let (Ok(token), Ok(url)) = (
-                            serde_wasm_bindgen::from_value::<String>(token),
-                            serde_wasm_bindgen::from_value::<String>(payload),
-                        ) {
-                            link.send_message(Msg::FilePicked { token, url });
-                        }
-                    }) as Box<dyn Fn(JsValue, JsValue)>);
-
-                    if let Err(err) = create_picker(&cb).await {
-                        log::error!("create_picker error: {:?}", err);
-                    }
-                    cb.forget();
-                });
-                false
-            }
             Msg::UpdateContext(auth_status) => {
                 self.auth_status = auth_status;
                 let page = self
@@ -433,12 +281,12 @@ impl Component for CreateLensPage {
                     </div>
                 </div>
                 <div class="flex flex-col gap-8 px-8 py-4">
-                    <AddSourceComponent />
+                    <AddSourceComponent lens_identifier={self.lens_identifier.clone()} />
                     {if let Some(paginator) = self.lens_source_paginator.clone() {
                         html! {
                             <div class="flex flex-col">
                                 <div class="flex flex-row items-center justify-between border-b border-neutral-700 pb-2">
-                                    <div class="font-bold">{format!("Sources ({})", paginator.num_items)}</div>
+                                    <div class="font-bold">{format!("Data Sources ({})", paginator.num_items)}</div>
                                     <Btn size={BtnSize::Sm} onclick={link.callback(move |_| Msg::ReloadSources(paginator.page))}>
                                         <icons::RefreshIcon
                                             classes="mr-1"
@@ -472,7 +320,6 @@ impl Component for CreateLensPage {
         }
     }
 }
-
 
 #[derive(Properties, PartialEq)]
 struct LensSourceComponentProps {
@@ -529,33 +376,5 @@ fn lens_source_comp(props: &LensSourceComponentProps) -> Html {
             </div>
 
         </div>
-    }
-}
-
-async fn add_lens_source(
-    api: &ApiClient,
-    new_source: &LensAddDocument,
-    identifier: &str,
-    link: Scope<CreateLensPage>,
-) {
-    if let Err(err) = api.lens_add_source(identifier, new_source).await {
-        log::error!("error adding url source: {err}");
-        match err {
-            ApiError::ClientError(msg) => {
-                link.send_message_batch(vec![Msg::Processing(None), Msg::AddUrlError(msg.message)])
-            }
-            _ => link.send_message_batch(vec![
-                Msg::Processing(None),
-                Msg::AddUrlError(err.to_string()),
-            ]),
-        };
-    } else {
-        link.send_message(Msg::ClearUrlError);
-        // Reload data if successful
-        link.send_message_batch(vec![
-            Msg::Processing(None),
-            Msg::ClearUrlError,
-            Msg::ReloadSources(0),
-        ]);
     }
 }
