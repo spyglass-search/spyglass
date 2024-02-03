2eccd43f4467239901b8e20c2e10aa8809a547ca
Dr. Chat <arkolbed@gmail.com>
2024-02-03 13:07:44 -0600

    `cargo build --all` works now

--- apps/web/build.rs
+++ apps/web/build.rs
@@ -1,5 +1,22 @@
 // build.rs
-use std::process::Command;
+use std::{
+    path::{Path, PathBuf},
+    process::Command,
+};
+
+// https://stackoverflow.com/questions/43577885/is-there-a-cargo-environment-variable-for-the-workspace-directory
+fn workspace_dir() -> PathBuf {
+    let output = std::process::Command::new(env!("CARGO"))
+        .arg("locate-project")
+        .arg("--workspace")
+        .arg("--message-format=plain")
+        .output()
+        .unwrap()
+        .stdout;
+    let cargo_path = Path::new(std::str::from_utf8(&output).unwrap().trim());
+    cargo_path.parent().unwrap().to_path_buf()
+}
+
 fn main() {
     let output = Command::new("git")
         .args(["rev-parse", "--short", "HEAD"])
@@ -9,6 +26,13 @@ fn main() {
     } else {
         String::from("N/A")
     };
+
+    let workspace = workspace_dir();
+
+    // Check for the .env file, and if not present, copy in the template.
+    if !workspace.join(".env").exists() {
+        std::fs::copy(workspace.join(".env.template"), workspace.join(".env")).unwrap();
+    }
 
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
 }
