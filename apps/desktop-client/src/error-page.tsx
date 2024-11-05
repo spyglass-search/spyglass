d6bfbef62adcd4c5b525c040e5b0684a7e385ead
Andrew Huynh <andrew@spyglass.fyi>
2024-11-05 12:07:48 -0800

    fixing type errors

--- apps/desktop-client/src/error-page.tsx
+++ apps/desktop-client/src/error-page.tsx
@@ -1,14 +1,14 @@
-import { useRouteError } from "react-router-dom";
+import { ErrorResponse, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
-  const error = useRouteError();
+  const error = useRouteError() as Error | ErrorResponse;
   console.error(error);
   return (
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{"statusText" in error ? error.statusText : error.message}</i>
       </p>
     </div>
   );
