190051aefe09ec7764ff37920e1c120bd83a2286
Andrew Huynh <andrew@spyglass.fyi>
2024-11-18 11:51:36 -0800

    prettier

--- apps/desktop-client/src/components/CustomTitleBar.tsx
+++ apps/desktop-client/src/components/CustomTitleBar.tsx
@@ -4,7 +4,7 @@ import { getOperatingSystem, OperatingSystem } from "../utils";
 import { XMarkIcon } from "@heroicons/react/16/solid";
 
 interface Props {
-  osStyle?: OperatingSystem
+  osStyle?: OperatingSystem;
 }
 
 export function CustomTitleBar({ osStyle = getOperatingSystem() }: Props) {
@@ -22,20 +22,32 @@ export function CustomTitleBar({ osStyle = getOperatingSystem() }: Props) {
       "flex",
       "flex-row",
       "justify-center",
-      "group-hover:bg-red-500"
+      "group-hover:bg-red-500",
     ];
     if (osStyle === OperatingSystem.MacOS) {
       return (
         <div className="ml-[8px] group">
-          <button className={classNames(baseStyles, "btn-circle bg-neutral w-[12px] h-[12px]")} onClick={handleClose}>
-            <XMarkIcon className="w-[10px] ml-[1px] text-neutral group-hover:text-black"/>
+          <button
+            className={classNames(
+              baseStyles,
+              "btn-circle bg-neutral w-[12px] h-[12px]",
+            )}
+            onClick={handleClose}
+          >
+            <XMarkIcon className="w-[10px] ml-[1px] text-neutral group-hover:text-black" />
           </button>
         </div>
       );
     } else {
       return (
         <div className="group">
-          <div className={classNames(baseStyles, "w-[30px] h-[30px] bg-neutral-900")} onClick={handleClose}>
+          <div
+            className={classNames(
+              baseStyles,
+              "w-[30px] h-[30px] bg-neutral-900",
+            )}
+            onClick={handleClose}
+          >
             <XMarkIcon className="w-[16px] text-neutral-400 group-hover:text-black" />
           </div>
         </div>
@@ -44,16 +56,20 @@ export function CustomTitleBar({ osStyle = getOperatingSystem() }: Props) {
   };
 
   return (
-    <div data-tauri-drag-region
-      className={classNames(["titlebar", "flex", "flex-row", "bg-neutral-900", {
-        "place-content-end": osStyle !== OperatingSystem.MacOS,
-        "place-content-start": osStyle === OperatingSystem.MacOS
-      }])}
+    <div
+      data-tauri-drag-region
+      className={classNames([
+        "titlebar",
+        "flex",
+        "flex-row",
+        "bg-neutral-900",
+        {
+          "place-content-end": osStyle !== OperatingSystem.MacOS,
+          "place-content-start": osStyle === OperatingSystem.MacOS,
+        },
+      ])}
     >
       {renderButton()}
     </div>
   );
 }
-
-
-
