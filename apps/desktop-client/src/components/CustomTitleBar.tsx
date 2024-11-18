2580a506f65de43e276c050a14f518f0fe2e086b
Andrew Huynh <andrew@spyglass.fyi>
2024-11-18 11:36:41 -0800

    adding in support for linux/windows custom title bar

--- apps/desktop-client/src/components/CustomTitleBar.tsx
+++ apps/desktop-client/src/components/CustomTitleBar.tsx
@@ -3,31 +3,48 @@ import classNames from "classnames";
 import { getOperatingSystem, OperatingSystem } from "../utils";
 import { XMarkIcon } from "@heroicons/react/16/solid";
 
-export function CustomTitleBar() {
+interface Props {
+  osStyle?: OperatingSystem
+}
+
+export function CustomTitleBar({ osStyle = getOperatingSystem() }: Props) {
   const appWindow = getCurrentWindow();
-  const os = getOperatingSystem();
-
-  const condClasses = {
-    "flex-end": os === OperatingSystem.Windows || os === OperatingSystem.Linux,
-    "flex-start": os === OperatingSystem.MacOS
-  };
-
   const handleClose = () => {
-    if (os === OperatingSystem.MacOS) {
+    if (osStyle === OperatingSystem.MacOS) {
       appWindow.hide();
     } else {
       appWindow.close();
     }
   };
 
+  const renderButton = () => {
+    if (osStyle === OperatingSystem.MacOS) {
+      return (
+        <div className="ml-[8px] group">
+          <button className="btn-circle bg-neutral w-[12px] h-[12px] group-hover:bg-red-500" onClick={handleClose}>
+            <XMarkIcon className="w-[10px] ml-[1px] text-neutral group-hover:text-black"/>
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="group">
+          <div className="flex flex-row items-center justify-center w-[30px] h-[30px] bg-neutral-900 hover:bg-red-500">
+            <XMarkIcon className="w-[16px] text-neutral-400 group-hover:text-black" />
+          </div>
+        </div>
+      );
+    }
+  };
+
   return (
     <div data-tauri-drag-region
-      className={classNames(["titlebar", "flex", "bg-neutral-900", condClasses])}>
-      <div className="ml-[8px] group">
-        <button className="btn-circle bg-neutral w-[12px] h-[12px] group-hover:bg-red-500" onClick={handleClose}>
-          <XMarkIcon className="w-[10px] ml-[1px] text-neutral group-hover:text-black"/>
-        </button>
-      </div>
+      className={classNames(["titlebar", "flex", "flex-row", "bg-neutral-900", {
+        "place-content-end": osStyle !== OperatingSystem.MacOS,
+        "place-content-start": osStyle === OperatingSystem.MacOS
+      }])}
+    >
+      {renderButton()}
     </div>
   );
 }
